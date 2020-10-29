22c9bfcf33a2bd279c4118f46889513c85944281
Michael <michael.kihm@web.de>
2020-10-29 09:33:33 +0100

    added layout margin

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 export default function Layout(props) {
   return (
     <Fragment>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" className={'mb-2'}>
         <Navbar.Brand href="#home">Travelmate</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
