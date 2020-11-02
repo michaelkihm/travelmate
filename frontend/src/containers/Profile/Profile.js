b85f875a2fbc4b65b269ebf60412ca5f81023eae
Michael <michael.kihm@web.de>
2020-11-02 15:30:42 +0100

    Added <Profile /> structure

--- frontend/src/containers/Profile/Profile.js
+++ frontend/src/containers/Profile/Profile.js
@@ -1,7 +1,57 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+import ProfileInfo from '../../components/ProfileInfo/ProfileInfo';
+import NextTravels from '../../components/NextTravels/NextTravels';
+import Interests from '../../components/Interests/Interests';
+import Posts from '../../components/Posts/Posts';
+
+const Travels = ['India', 'Spain', 'Italy'];
+const profImg =
+  'https://img.welt.de/img/politik/ausland/mobile207593871/5432503607-ci102l-w1300/Bill-Gates-in-Brussels-to-Promote-Health-and-Clean-Energy-Initiatives.jpg';
+const inter = ['surfing', 'climbing', 'jogging', 'swimming'];
+
+const posts = [
+  { user: 'John', body: 'I will go to India' },
+  { user: 'test2', body: 'I will go to Austria' },
+  {
+    user: 'Mickenhos',
+    body: 'I will go to the USA',
+    img: 'https://s.hdnux.com/photos/01/11/47/50/19298838/3/1200x0.jpg',
+  },
+];
 
 export default class Profile extends Component {
+  state = {
+    profileImg: profImg,
+    nextTravels: Travels,
+    interests: inter,
+    name: 'Bill Gates',
+    infoText: "Hello I'm the guy behind Windows OS",
+    posts: posts,
+  };
   render() {
-    return <div>Hello</div>;
+    return (
+      <Container fluid>
+        <Row>
+          <Col sm={2.5} className="bg-info">
+            <ProfileInfo
+              image={this.state.profileImg}
+              name={this.state.name}
+              infoText={this.state.infoText}
+            />
+            <NextTravels nextTravels={this.state.nextTravels} />
+            <Interests interests={this.state.interests} />
+          </Col>
+          <Col>Map</Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <Posts posts={this.state.posts} />
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
---
+++ frontend/src/containers/Profile/Profile.test.js
@@ -0,0 +1,43 @@
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import Profile from './Profile';
+import ProfileInfo from '../../components/ProfileInfo/ProfileInfo';
+import NextTravels from '../../components/NextTravels/NextTravels';
+import Interests from '../../components/Interests/Interests';
+import Posts from '../../components/Posts/Posts';
+
+configure({ adapter: new Adapter() });
+
+describe('<Profile />', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Profile />);
+  });
+
+  it('Should include 2 <Row />', () => {
+    expect(wrapper.find(Row)).toHaveLength(2);
+  });
+
+  it('Should include 3 <Col />', () => {
+    expect(wrapper.find(Col)).toHaveLength(3);
+  });
+
+  it('Should include one <ProfileInfo />', () => {
+    expect(wrapper.find(ProfileInfo)).toHaveLength(1);
+  });
+
+  it('Should include one <NextTravels />', () => {
+    expect(wrapper.find(NextTravels)).toHaveLength(1);
+  });
+
+  it('Should include one <Interests />', () => {
+    expect(wrapper.find(Interests)).toHaveLength(1);
+  });
+
+  it('Should include one <Posts />', () => {
+    expect(wrapper.find(Posts)).toHaveLength(1);
+  });
+});
