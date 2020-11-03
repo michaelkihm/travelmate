6d6c3b9ad017fbb6afde0e58a3e15c4093469cbd
Michael <michael.kihm@web.de>
2020-11-03 17:23:51 +0100

    corrected typo

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ export default function NotFound() {
     <Jumbotron>
       <h1>404</h1>
       <h2>UH OH! You're lost</h2>
-      <p>This page you are looking does not exist!</p>
+      <p>This page you are looking for does not exist! :(</p>
     </Jumbotron>
   );
 }
